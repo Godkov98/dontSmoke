fd0c57112596b1020fe8bfbf459502deb5d008d2
Georgy Godkov <godkov98@gmail.com>
2020-04-13 11:09:04 +0300

    V 1.1

--- app/src/main/java/com/example/andr6buttons/MainActivity.java
+++ app/src/main/java/com/example/andr6buttons/MainActivity.java
@@ -6,14 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
+    private TextView thedate;
+    private Button buttonCalendar;
     Button buttonData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        thedate = (TextView) findViewById(R.id.buttonCalendar);
+        
         buttonData = (Button) findViewById(R.id.buttonData);
         buttonData.setOnClickListener((View.OnClickListener) this);
     }
